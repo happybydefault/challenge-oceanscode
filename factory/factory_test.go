fa7343b84e4536eefdbd45e5bbf8889051617668
Raúl Garrido <me@happybydefault.com>
2021-08-25 23:49:34 -0400

    Implement unit testing for Factory struct

--- factory/factory_test.go
+++ factory/factory_test.go
@@ -24,3 +24,23 @@ func (s *factoryUnitTestSuite) TestSample() {
 	// Assert
 	s.Assert().Equal(1, 1)
 }
+
+func (s *factoryUnitTestSuite) TestStartAssemblingProcessPositive() {
+	amount := 13
+
+	ch := s.adapter.StartAssemblingProcess(amount)
+
+	for v := range ch {
+		s.Require().NotEmpty(v)
+
+		assembled := "Assembled"
+		s.Require().Equal(assembled, v.Chassis)
+		s.Require().Equal(assembled, v.Tires)
+		s.Require().Equal(assembled, v.Engine)
+		s.Require().Equal(assembled, v.Electronics)
+		s.Require().Equal(assembled, v.Dash)
+		s.Require().Equal(assembled, v.Sits)
+		s.Require().Equal(assembled, v.Windows)
+		s.Require().Equal(assembled, v.Windows)
+	}
+}
